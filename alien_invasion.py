59b55b619382ed113d89e5d3d7edb06fcea9946e
Alex <grishunion@gmail.com>
2025-09-12 14:40:20 +0400

    Added ship, sittings

---
+++ alien_invasion.py
@@ -0,0 +1,50 @@
+#Создание окна Pygame и обработка ввода
+import sys 
+
+import pygame
+from settings import Settings
+from ship import Ship
+
+
+class AlienInvasion:
+    """Класс для управления ресурсами и поведением игры"""
+
+    def __init__(self):
+        """Инициализирует игру и создает игровые ресурсы"""
+        pygame.init()
+        self.settings = Settings()
+        
+        self.screen = pygame.display.set_mode(
+            (self.settings.screen_width, self.settings.screen_hight))
+        pygame.display.set_caption("Alien Invasion")
+
+        self.ship = Ship(self)
+        
+           
+    def run_game(self):
+        """Запуск основного цикла игры."""
+        while True:
+            self._check_events()
+            self._update_screen()
+
+            #Отображение последнего прорисованного экрана.
+            pygame.display.flip()
+
+    def _check_events(self):
+        #Отслеживание событий клавиатуры и мыши.
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    sys.exit()    
+
+    def _update_screen(self):
+        """Обновляет изображение на экране и отображает новый экран."""
+        
+        self.screen.fill(self.settings.bg_color)
+        self.ship.blitme()
+
+if __name__ == '__main__':
+    #Создание экземпляра и запуск игры
+    ai = AlienInvasion()
+    ai.run_game()                         
+
+
