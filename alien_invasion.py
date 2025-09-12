d286559f4ae18b1b5c1631b38f16b12f434f9de5
Alex <grishunion@gmail.com>
2025-09-12 15:12:53 +0400

    Added bottom control ship(left, right)

--- alien_invasion.py
+++ alien_invasion.py
@@ -25,6 +25,7 @@ class AlienInvasion:
         """Запуск основного цикла игры."""
         while True:
             self._check_events()
+            self.ship.update()
             self._update_screen()
 
             #Отображение последнего прорисованного экрана.
@@ -35,10 +36,21 @@ class AlienInvasion:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     sys.exit()    
+                elif event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = True
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = True    
+
+                elif event.type == pygame.KEYUP:
+                    if event.key == pygame.K_RIGHT:
+                        self.ship.moving_right = False  
+                    elif event.key == pygame.K_LEFT:
+                        self.ship.moving_left = False             
 
     def _update_screen(self):
         """Обновляет изображение на экране и отображает новый экран."""
-        
+    
         self.screen.fill(self.settings.bg_color)
         self.ship.blitme()
 
